3561dc329f494364b1f18622a8b4cf4390865e17
Zachary Murphy <murphyz@my.wccc.edu>
2016-03-04 16:47:29 -0500

    nothing will show now... :(

--- Slideshow/src/Slideshow.java
+++ Slideshow/src/Slideshow.java
@@ -11,6 +11,7 @@ import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import static java.nio.channels.AsynchronousFileChannel.open;
@@ -22,8 +23,10 @@ import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.Timer;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import static sun.awt.image.ImagingLib.filter;
@@ -42,23 +45,49 @@ public class Slideshow extends JApplet  {
     private JPanel picturePanel;
     private JPanel navigationPanel;
     private JPanel mainPanel;
+    private BufferedImage pics;
+    private int counter = 0;
+    private ImageIcon[] pictures ;
+    private JLabel label;
     
     int currentImage = -1;
     String images[];
     private Object image;
+    File selectedFile;
+    String fileName;
     
 
   
     public void init() {
-        this.setSize(500,500);
+        this.setSize(1000,1500);
         buildNavPanel();
+        buildPicturePanel();
         mainPanel();
         
         
     }
     
     private void buildPicturePanel(){
-        picturePanel = new JPanel();
+        
+        for (int i = 0; i< pictures.length; ++i)
+            pictures[counter] = new ImageIcon(fileName);
+        
+        label = new JLabel();
+        
+        Timer timer = new Timer(100, new TimerListener());
+ 
+
+        
+    }
+    
+    class TimerListener implements ActionListener{
+
+       
+        public void actionPerformed(ActionEvent e) {
+            counter ++;
+            counter %= pictures.length;
+            label.setIcon(pictures[counter]);
+        }
         
     }
     private void buildNavPanel(){
@@ -75,10 +104,13 @@ public class Slideshow extends JApplet  {
         
         
     }
+   
     
     private void mainPanel(){
         buildNavPanel();
+        buildPicturePanel();
         add(navigationPanel,BorderLayout.SOUTH);
+        add(picturePanel, BorderLayout.CENTER);
     }
     public void showCurrent(){
         display.setIcon(new ImageIcon(images[currentImage]));
@@ -109,8 +141,8 @@ public class Slideshow extends JApplet  {
     fc.setMultiSelectionEnabled(true);
      int response = fc.showOpenDialog(null);
      if (response == JFileChooser.APPROVE_OPTION){
-        File selectedFile = fc.getSelectedFile();
-        String fileName = selectedFile.getPath();
+         selectedFile = fc.getSelectedFile();
+         fileName = selectedFile.getPath();
   
     }
 
