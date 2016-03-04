c573e4a294ad93d0e76d7a8f38e80bfd13282036
Zachary Murphy <murphyz@my.wccc.edu>
2016-03-03 20:29:52 -0500

    getting a multiple images selection...stuck

--- Slideshow/src/Slideshow.java
+++ Slideshow/src/Slideshow.java
@@ -4,21 +4,71 @@
  * and open the template in the editor.
  */
 
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.FlowLayout;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.nio.file.Files;
 import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  *
  * @author Zachary Murphy
  */
-public class Slideshow extends JApplet {
 
-    /**
-     * Initialization method that will be called after the applet is loaded into
-     * the browser.
-     */
+public class Slideshow extends JApplet implements ActionListener {
+   
+    JLabel display = new JLabel();
+    JButton open = new JButton("Open");
+    JButton start = new JButton ("Start");
+    JButton stop = new JButton ("Stop");
+    
+    int currentImage = -1;
+    String images[];
+    
+
+  
     public void init() {
-        // TODO start asynchronous download of heavy resources
+        JPanel buttons = new JPanel (new FlowLayout());
+        buttons.add(open);
+        buttons.add(start);
+        buttons.add(stop);
+        
+        getContentPane().setLayout(new BorderLayout());
+        getContentPane().add("Center", display);
+        getContentPane().add("South", buttons);
+        
+        
+    }
+    public void showCurrent(){
+        
+    }
+    public void showNext(){
+        
+    }
+    
+   
+
+   
+    public void actionPerformed(ActionEvent e) {
+        int x =0;
+        if (e.getSource() == open){
+            JFileChooser fc = new JFileChooser();
+            int result = fc.showOpenDialog(null);
+            if (result == JFileChooser.APPROVE_OPTION){
+                x=x+1;
+                Files[x] images = fc.getSelectedFile();
+            }
+        }
     }
 
-    // TODO overwrite start(), stop() and destroy() methods
+
+
 }
