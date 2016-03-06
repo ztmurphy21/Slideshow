4453abade7a91346de0e8992710db732e17c6aae
Zachary Murphy <murphyz@my.wccc.edu>
2016-03-06 13:48:01 -0500

    it is finished!!! :)

--- Slideshow/src/Slideshow.java
+++ Slideshow/src/Slideshow.java
@@ -11,6 +11,7 @@ import java.io.File;
  */
 
 public class Slideshow extends JFrame  {
+    //declaration of all variables 
     private final int MAX_IMAGES = 10;
     private JPanel imagePanel;
     private JPanel buttonPanel; 
@@ -26,51 +27,91 @@ public class Slideshow extends JFrame  {
     private int numImages = 0;
     private int currentImage = 0;
     
+    //constructor for slide show
 public Slideshow(){
-    setTitle("Slide show");
+    setTitle("Slide show");//title 
     
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//close setting
     
+    
+    //border layout 
     setLayout(new BorderLayout());
     
+    
+    //this will bring panels in
      buildImagePanel();
     buildButtonPanel();
     
+    
+    //locations set in border layout for each panel
     add(imagePanel, BorderLayout.CENTER);
     add(buttonPanel, BorderLayout.SOUTH);
     
+    
+    //inst of fileSelector id
     fileSelector = new JFileChooser(".");
     
+    //inst of timer/iamges id
     timer = new Timer(timeDelay, new TimerListener());
     images = new ImageIcon[MAX_IMAGES];
     pack();
+    
+    //will make everything displayed
     setVisible(true);
     
 }
+
+/**
+ * This method is for the ImageJPanel
+ */
 private void buildImagePanel(){
+    
+    //adding panel and label
     imagePanel = new JPanel();
+    imageLabel = new JLabel();
+    
+    
+    //making visible in panel
+    imagePanel.add(imageLabel);
     
 }
+
+/**
+ * This method is for the button  JPanel
+ */
     private void buildButtonPanel(){
         buttonPanel = new JPanel();
         
+        //add image button
+        imageButton = new JButton ("Add Image");
+        imageButton.setMnemonic(KeyEvent.VK_A);
+        imageButton.setToolTipText("Click here to add an image.");
+        
+        
+        //delay button
         delayButton = new JButton("Set Time Delay");
         delayButton.setMnemonic(KeyEvent.VK_T);
         delayButton.setToolTipText("Click here to set the time delay.");
         
+        //start button
         startButton = new JButton("Start");
         startButton.setMnemonic(KeyEvent.VK_S);
         startButton.setToolTipText("Click here to start the slide show.");
         
+        //stop button
         stopButton = new JButton ("Stop");
         stopButton.setMnemonic(KeyEvent.VK_P);
         stopButton.setToolTipText("Click here to stop the slide show.");
         
+        
+        //actions listeners for all butotns
         imageButton.addActionListener(new ImageButtonListener());
         delayButton.addActionListener(new DelayButtonListener());
         startButton.addActionListener(new StartButtonListener());
         stopButton.addActionListener(new StopButtonListener());
         
+        
+        //adding all buttons to be visble
         buttonPanel.add(imageButton);
         buttonPanel.add(delayButton);
         buttonPanel.add(startButton);
@@ -81,6 +122,9 @@ private void buildImagePanel(){
     
     }
     
+    /**
+     * This method implements  action Listener for the add image button
+     */
     private class ImageButtonListener implements ActionListener{
         
         @Override
@@ -108,7 +152,9 @@ private void buildImagePanel(){
         }
         
     }
-    
+    /**
+     * This method implements action listener for if delay button is selected
+     */
     private class DelayButtonListener implements ActionListener{
 
         @Override
@@ -119,6 +165,10 @@ private void buildImagePanel(){
             
         }
     }
+    
+    /**
+     * This method is for when start button is selected
+     */
         
         private class StartButtonListener implements ActionListener{
 
@@ -130,6 +180,9 @@ private void buildImagePanel(){
             
         }
         
+        /**
+         * This method is for when the stop button is selected
+         */
         private class StopButtonListener implements ActionListener{
 
             @Override
@@ -138,6 +191,10 @@ private void buildImagePanel(){
                 }
             
         }
+        
+        /**
+         * This method is for when the timer button is slected
+         */
        private class TimerListener implements ActionListener{
 
             @Override
@@ -155,6 +212,11 @@ private void buildImagePanel(){
            
        }
         
+       
+       /** m
+        * main method of program
+        * @param args 
+        */
     
     public static void main (String[] args){
         Slideshow ss = new Slideshow();
