6a33bfd378e2bcb705ac3ce0117c32aba031117c
Zachary Murphy <murphyz@my.wccc.edu>
2016-03-05 23:36:31 -0500

    on its way..

--- Slideshow/src/Slideshow.java
+++ Slideshow/src/Slideshow.java
@@ -1,137 +1,163 @@
-import java.awt.BorderLayout;
-import java.awt.Canvas;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 import java.io.File;
-import java.io.IOException;
-import static java.nio.channels.AsynchronousFileChannel.open;
-import java.nio.file.Files;
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JApplet;
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.Timer;
-import javax.swing.filechooser.FileFilter;
-import javax.swing.filechooser.FileNameExtensionFilter;
-import static sun.awt.image.ImagingLib.filter;
+
+
 
 /**
  *
  * @author Zachary Murphy
  */
 
-public class Slideshow extends JApplet  {
-   //setting all JItmes along with items for the image and timer
-    private JLabel display;
-    private JButton open ;
-   private  JButton start ;
-    private JButton stop ;
-    private JPanel picturePanel;
-    private JPanel navigationPanel;
-    private JPanel mainPanel;
-    private BufferedImage pics;
-    private int counter = 0;
-    private ImageIcon[] pictures ;
-    private JLabel label;
+public class Slideshow extends JFrame  {
+    private final int MAX_IMAGES = 10;
+    private JPanel imagePanel;
+    private JPanel buttonPanel; 
+    private JLabel imageLabel;
+    private JButton imageButton;
+    private JButton delayButton;
+    private JButton startButton;
+    private JButton stopButton;
+    private  JFileChooser fileSelector;
+    private Timer timer;
+    private int timeDelay = 1000;
+    private ImageIcon[] images;
+    private int numImages = 0;
+    private int currentImage = 0;
     
-    int currentImage = -1;
-    String images[];
-    private Object image;
-    File selectedFile;
-    String fileName;
+public Slideshow(){
+    setTitle("Slide show");
     
-
-  //basically the main method for a JApplet
-    public void init() {
-        this.setSize(400,500);
-        buildNavPanel();
-       buildPicturePanel();
-        mainPanel();
-        
-        
-    }
-    //creates the panel that will hold the slide show
-    private void buildPicturePanel(){
-      try{
-          pics = ImageIO.read(new File(fileName));
-      }catch(IOException ex){
-          System.out.println("Please open another selection of pictures.");
-      }
-        
-    }
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     
-    //navigation panel where user can hit add, start, stop
-    private void buildNavPanel(){
-        navigationPanel = new JPanel(new FlowLayout(FlowLayout.TRAILING));
-        navigationPanel.setLayout(new GridLayout(0,3));
-        open = new JButton("Open Images (Up to 10)");
-        start = new JButton ("Start Slide Show");
-        stop = new JButton ("Stop Slide Show");
-        open.addActionListener(new OpenListener());
-        navigationPanel.add(open);
-        navigationPanel.add(start);
-        navigationPanel.add(stop);
-        
-        
-        
-    }
-   
-    //main panel holds all panels with location
-    private void mainPanel(){
-        buildNavPanel();
-       buildPicturePanel();
-        add(navigationPanel,BorderLayout.SOUTH);
-        add(picturePanel, BorderLayout.CENTER);
-    }
+    setLayout(new BorderLayout());
     
-    //shows current picture
-    public void showCurrent(){
-        display.setIcon(new ImageIcon(images[currentImage]));
+  //  buildImagePanel();
+    buildButtonPanel();
+    
+    add(imagePanel, BorderLayout.CENTER);
+    add(buttonPanel, BorderLayout.SOUTH);
+    
+    fileSelector = new JFileChooser(".");
+    
+    timer = new Timer(timeDelay, new TimerListener());
+    images = new ImageIcon[MAX_IMAGES];
+    pack();
+    setVisible(true);
+    
+}
+    private void buildButtonPanel(){
+        buttonPanel = new JPanel();
         
+        delayButton = new JButton("Set Time Delay");
+        delayButton.setMnemonic(KeyEvent.VK_T);
+        delayButton.setToolTipText("Click here to set the time delay.");
+        
+        startButton = new JButton("Start");
+        startButton.setMnemonic(KeyEvent.VK_S);
+        startButton.setToolTipText("Click here to start the slide show.");
+        
+        stopButton = new JButton ("Stop");
+        stopButton.setMnemonic(KeyEvent.VK_P);
+        stopButton.setToolTipText("Click here to stop the slide show.");
+        
+        imageButton.addActionListener(new ImageButtonListener());
+        delayButton.addActionListener(new DelayButtonListener());
+        startButton.addActionListener(new StartButtonListener());
+        stopButton.addActionListener(new StopButtonListener());
+        
+        buttonPanel.add(imageButton);
+        buttonPanel.add(delayButton);
+        buttonPanel.add(startButton);
+        buttonPanel.add(stopButton);
+        
+        
+        
+    
     }
     
-    //shows next picture
-    public void showNext(){
-        currentImage = currentImage +1;
-        if (currentImage >= images.length){
-            currentImage = 0;
+    private class ImageButtonListener implements ActionListener{
+        
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+               File selectedFile;
+               ImageIcon thisImage;
+               String filename;
+               int fileChooserStatus;
+               
+              if (numImages >=MAX_IMAGES){
+                  JOptionPane.showMessageDialog(null, "The maximum number of images" + " have been selected. ");
+              }else{
+                  fileChooserStatus = fileSelector.showOpenDialog(Slideshow.this);
+                  
+              if (fileChooserStatus == JFileChooser.APPROVE_OPTION){
+                  selectedFile = fileSelector.getSelectedFile();
+                  filename= selectedFile.getPath();
+                  thisImage = new ImageIcon(filename);
+                  images[numImages] = thisImage;
+                  numImages++;
+                  
+              }
+              }
+            
         }
-        showCurrent();
-    
-        
         
     }
+    
+    private class DelayButtonListener implements ActionListener{
 
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            String input = JOptionPane.showInputDialog("Enter the time delay (in seconds).");
+            timeDelay = Integer.parseInt(input) * 1000;
+            timer.setDelay(timeDelay);
+            
+        }
+    }
+        
+        private class StartButtonListener implements ActionListener{
 
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                timer.start();
+                
+            }
+            
+        }
+        
+        private class StopButtonListener implements ActionListener{
+
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                timer.stop();
+                }
+            
+        }
+       private class TimerListener implements ActionListener{
+
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                setTitle("Image " + String.valueOf(currentImage +1));
+                imageLabel.setIcon(images[currentImage]);
+                pack();
+                
+                if(currentImage < (numImages -1)){
+                    currentImage++;
+                }else{
+                    currentImage =0;
+                }
+            }
+           
+       }
+        
+    
+    public static void main (String[] args){
+        Slideshow ss = new Slideshow();
+    }
+}
+    
     
    
-//this will make the openlistener work
-   private class OpenListener implements ActionListener{
-       
-   //actionevent for openlistener
-    public void actionPerformed(ActionEvent e) {
-        JFileChooser fc= new JFileChooser();
-     FileFilter filter = new FileNameExtensionFilter ("JPEG file", "jpg", "jpeg");
-    fc.setFileFilter(filter);
-    fc.setMultiSelectionEnabled(true);
-     int response = fc.showOpenDialog(null);
-     if (response == JFileChooser.APPROVE_OPTION){
-         selectedFile = fc.getSelectedFile();
-         fileName = selectedFile.getPath();
-  
-    }
 
-    }
 
-}
-}
